e50b63d0a2dcc67a5afc7482ff524717c7da504f
Alina-Kulshik <equilibrium1.3@mail.ru>
2020-10-11 20:33:48 +0300

    stepik_project_2week

--- tours/views.py
+++ tours/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from random import sample
+
 from django.http import HttpResponseNotFound, Http404
+from django.shortcuts import render
 
 from tours import data
 
@@ -8,25 +10,52 @@ def index(request):
     title = data.title
     subtitle = data.subtitle
     description = data.description
+    departures = data.departures
+    tours = {i: data.tours[i] for i in sample(range(1, 17), 6)}
     return render(request, 'index.html', context={
         'title': title,
         'subtitle': subtitle,
         'description': description,
+        'departures': departures,
+        'tours': tours,
     })
 
 
 def departure(request, departure):
+    tours = {i: data.tours[i] for i in data.tours if data.tours[i]['departure'] == departure}
+    price = [tours[i]['price'] for i in tours]
+    min_price = min(price)
+    max_price = max(price)
+    nights = [tours[i]['nights'] for i in tours]
+    min_nights = min(nights)
+    max_nights = max(nights)
     departure = data.departures.get(departure)
+    departures = data.departures
     if departure is None:
         raise Http404
-    return render(request, 'departure.html', context={'departure': departure})
+    return render(request, 'departure.html', context={
+        'departure': departure,
+        'departures': departures,
+        'tours': tours,
+        'min_price': min_price,
+        'max_price': max_price,
+        'min_nights': min_nights,
+        'max_nights': max_nights,
+    })
 
 
 def tour(request, tour_id):
     tour = data.tours.get(tour_id)
+    departure = data.tours[tour_id]['departure']
+    departure = data.departures[departure]
+    departures = data.departures
     if tour is None:
         raise Http404
-    return render(request, 'tour.html', context={'tour': tour})
+    return render(request, 'tour.html', context={
+        'tour': tour,
+        'departure': departure,
+        'departures': departures,
+    })
 
 
 def custom_handler404(request, exception):
